4a22f1fa61d2c983a8e3892631c477e19f9ae05c
Helena Pedro <mbeua94@gmail.com>
2024-10-24 14:27:01 +0100

    defining the rate limiting configuration

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,19 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
-//import rateLimit from 'express-rate-limit';
+import rateLimit from 'express-rate-limit';
+import { rateLimitConfig } from './config';
 
 const app = express();
-
+const limiter = rateLimit(rateLimitConfig);
 
 // Middleware
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
+
+app.use(limiter);
 
 app.get('/', (req, res) => {
      res.send('Resume Feedback API');
