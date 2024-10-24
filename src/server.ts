83031d4230384267fe45bfc334b854985e96abfc
Helena Pedro <mbeua94@gmail.com>
2024-10-24 15:35:32 +0100

    updating server configuration

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import helmet from 'helmet';
 import cors from 'cors';
 import rateLimit from 'express-rate-limit';
 import { rateLimitConfig } from './config';
+import { errorHandler } from './middlewares/errorHandler';
+import { requestLogger, errorLogger } from './middlewares/logging';
 
 const app = express();
 const limiter = rateLimit(rateLimitConfig);
@@ -13,8 +15,14 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
+app.use(requestLogger);
+app.use(errorLogger);
 
 app.use(limiter);
+
+
+// Error Handling
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
      res.send('Resume Feedback API');
