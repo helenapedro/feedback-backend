a0bbcc924a5624728d8e41d14200330f0b6f2c82
Helena Pedro <mbeua94@gmail.com>
2024-10-25 20:24:32 +0100

    new commit

--- src/routes/commentRoutes.ts
+++ src/routes/commentRoutes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { authMiddleware } from '../middlewares/auth';
-import { addComment, getCommentsByResume } from '../controllers/commentController'; 
+import { addComment, getCommentsByResume, deleteComment } from '../controllers/commentController'; 
 
 const router = express.Router();
 
 router.post('/add', authMiddleware, addComment);
 
 router.get('/:resumeId', getCommentsByResume);
+
+router.delete('/:commentId', authMiddleware, deleteComment);
 
 export default router;
