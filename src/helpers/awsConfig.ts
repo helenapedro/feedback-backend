b382620d231bfb9f63686382c13fddc01768697f
Helena Pedro <mbeua94@gmail.com>
2024-10-25 15:02:16 +0100

    new commit

--- src/helpers/awsConfig.ts
+++ src/helpers/awsConfig.ts
@@ -4,10 +4,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'us-east-2'
 });
 
 const s3 = new AWS.S3();
+
 export default s3;
