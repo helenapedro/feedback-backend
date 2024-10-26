dae4064d01b4b67cec86bc3f7ff68199398980af
Helena Pedro <mbeua94@gmail.com>
2024-10-26 23:21:41 +0100

    new commit

--- src/helpers/mongoconn.ts
+++ src/helpers/mongoconn.ts
@@ -3,7 +3,7 @@ import logger from './logger';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_LOCALHOST || '');
+    const conn = await mongoose.connect(process.env.MONGO_URI || '');
     logger.info(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     if (error instanceof Error) {
