bd37f72d1a39203da6478ac912d59ab0c767ab88
Helena Pedro <mbeua94@gmail.com>
2024-10-29 10:42:32 +0100

    new commit

--- src/services/s3Service.ts
+++ src/services/s3Service.ts
@@ -30,7 +30,7 @@ const uploadToS3 = async (file: Express.Multer.File) => {
     Key: fileName,
     Body: file.buffer,
     ContentType: file.mimetype,
-    ContentDisposition: 'inline', 
+    /* ContentDisposition: 'inline',  */
   };
 
   try {
