572faae1f67e5b40352fdc2c1ab0035a6bfc71cc
Helena Pedro <mbeua94@gmail.com>
2024-10-29 13:07:15 +0100

    new commit

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -46,8 +46,12 @@ export const getCommentsByResume = async (req: Request, res: Response): Promise<
   }
 
   try {
-    const comments = await Comment.find({ resumeId, isDeleted: false }).populate('commenterId', 'username');
-    
+    const comments = await Comment.find({ resumeId, isDeleted: false })
+      .populate({ path: 'commenterId', select: 'username'})
+      .exec();
+      
+    console.log(comments);
+
     if (!comments || comments.length === 0) {
       res.status(404).json({ message: 'No comments found for this resume' });
       return;
