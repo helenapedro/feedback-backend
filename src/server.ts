9dc88a9332d339a8e820157fdbeae832b9d0f5a5
Helena Pedro <mbeua94@gmail.com>
2024-11-02 17:12:16 +0100

    new commit

--- src/server.ts
+++ src/server.ts
@@ -8,29 +8,28 @@ import commentRoutes from './routes/commentRoutes';
 
 const app = express();
 
-app.set('trust proxy', 1); 
+app.set('trust proxy', 1);
 
 const limiter = rateLimit(rateLimitConfig);
 
 const allowedOrigins = [
-     'http://localhost:3000',
-     'https://resumefeedback.mtcambrosio.com'
+  'http://localhost:3000',
+  'https://resumefeedback.mtcambrosio.com'
 ];
 
 const corsOptions = {
-     origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-       if (!origin || allowedOrigins.includes(origin)) {
-         callback(null, true); 
-       } else {
-         callback(new Error('Not allowed by CORS')); 
-       }
-     },
-     methods: ['GET', 'POST', 'PUT'],
-     allowedHeaders: ['Authorization', 'Content-Type'],
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT'],
+  allowedHeaders: ['Authorization', 'Content-Type'],
 };
 
 app.use(middlewares.cors(corsOptions));
-
 app.use(express.json());
 app.use(middlewares.morgan('dev'));
 app.use(middlewares.helmet());
@@ -38,17 +37,20 @@ app.use(middlewares.cors());
 app.use(middlewares.requestLogger);
 app.use(middlewares.errorLogger);
 app.use(middlewares.errorHandler);
-
-app.use(limiter); 
+app.use(limiter);
 
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/resumes', resumeRoutes);
 app.use('/api/comments', commentRoutes);
 
-
 app.get('/', (req, res) => {
-     res.send('Resume Feedback API');
+  res.send('Resume Feedback API');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
